2628425f530caadac6b85abb6a5966cccdf81181
BobbyLin23 <linzhangsheng23@gmail.com>
2024-06-09 21:33:47 +0800

    feat: add sidebar

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -1,4 +1,7 @@
 <script lang="ts" setup>
+import { ClerkLoaded, ClerkLoading, UserButton } from 'vue-clerk'
+import { Loader } from 'lucide-vue-next'
+
 import { cn } from '~/lib/utils'
 
 const attrs = useAttrs()
@@ -6,9 +9,32 @@ const attrs = useAttrs()
 
 <template>
   <div
-    :class="cn('bg-blue-50 h-full lg:w-[256px] lg:fixed flex left-0 top-0 px-4 border-r-2 flex-col',
+    :class="cn('h-full lg:w-[256px] lg:fixed flex left-0 top-0 px-4 border-r-2 flex-col',
                attrs.class as string)"
   >
-    Sidebar
+    <NuxtLink href="/learn">
+      <div class="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+        <NuxtImg src="/mascot.svg" height="40" width="40" alt="Mascot" />
+        <h1 class="text-2xl font-extrabold text-green-600 tracking-wide">
+          Lingo
+        </h1>
+      </div>
+    </NuxtLink>
+    <div class="flex flex-col gap-y-2 flex-1">
+      <SidebarItem label="Learn" href="/learn" icon-src="/learn.svg" />
+      <SidebarItem label="LeaderBoard" href="/leaderboard" icon-src="/leaderboard.svg" />
+      <SidebarItem label="quests" href="/quests" icon-src="/quests.svg" />
+      <SidebarItem label="shop" href="/shop" icon-src="/shop.svg" />
+    </div>
+    <ClientOnly>
+      <div class="p-4">
+        <ClerkLoading>
+          <Loader class="h-5 w-5 animate-spin text-muted-foreground" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton after-sign-out-url="/" />
+        </ClerkLoaded>
+      </div>
+    </ClientOnly>
   </div>
 </template>
---
+++ components/SidebarItem.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  label: string
+  iconSrc: string
+  href: string
+}>()
+
+const route = useRoute()
+
+const active = computed(() => route.path === props.href)
+</script>
+
+<template>
+  <Button :variant="active ? 'sidebarOutline' : 'sidebar'" class="justify-start h-[52px]">
+    <NuxtLink :href="href" class="flex items-center">
+      <NuxtImg :src="iconSrc" :alt="label" class="mr-5" height="32" width="32" />
+      {{ label }}
+    </NuxtLink>
+  </Button>
+</template>
